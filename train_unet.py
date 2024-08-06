13eb4bd8ecd35f91307043c137e16afd37307817
PomponWang <pxwang24@gmail.com>
2024-08-06 17:02:06 +0300

    update config

--- train_unet.py
+++ train_unet.py
@@ -22,10 +22,10 @@ def train_unet(unet_ck_dir, bnn_ck_path, layers, beta, input_weight_initial, inp
     trainloader, valloader = get_dataloader(data_dir=input_dir,
                                  train=True, val=True,
                                  batch_size=batch_size,
-                                 train_unet_ratio=0.5)
+                                 train_unet_ratio=0.3)
     
     bnn_model = StoResNet18(num_classes=n_classes, in_channels=in_channels, n_components=n_components, stochastic=1).to(device)
-    det_model = StoResNet18(num_classes=n_classes, in_channels=in_channels, n_components=n_components, stochastic=0).to(device)
+    det_model = StoResNet18(num_classes=n_classes, in_channels=in_channels, n_components=n_components, stochastic=2).to(device)
     
     model_dict = torch.load(bnn_ck_path)
     bnn_model.load_state_dict(model_dict)
